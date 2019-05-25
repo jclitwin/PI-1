4f0d209fccd41aff163e2b30762288b3dcbac9b4
Julio Litwin <j.litwin@live.com>
2019-05-25 16:45:06 -0300

    Removido alguns códigos de testes e adicionado ini-parser;

--- Teste/Teste/frmHome.Designer.cs
+++ Teste/Teste/frmHome.Designer.cs
@@ -28,23 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.panelControl2 = new DevExpress.XtraEditors.PanelControl();
             this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
-            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
-            this.memoEdit1 = new DevExpress.XtraEditors.MemoEdit();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).BeginInit();
             this.panelControl2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).BeginInit();
             this.SuspendLayout();
-            // 
-            // labelControl1
-            // 
-            this.labelControl1.Location = new System.Drawing.Point(207, 169);
-            this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(70, 13);
-            this.labelControl1.TabIndex = 0;
-            this.labelControl1.Text = "labelControl1";
             // 
             // panelControl2
             // 
@@ -70,47 +58,23 @@
             this.labelControl2.TabIndex = 0;
             this.labelControl2.Text = "Inicio";
             // 
-            // simpleButton1
-            // 
-            this.simpleButton1.Location = new System.Drawing.Point(100, 79);
-            this.simpleButton1.Name = "simpleButton1";
-            this.simpleButton1.Size = new System.Drawing.Size(200, 52);
-            this.simpleButton1.TabIndex = 5;
-            this.simpleButton1.Text = "simpleButton1";
-            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
-            // 
-            // memoEdit1
-            // 
-            this.memoEdit1.Location = new System.Drawing.Point(100, 244);
-            this.memoEdit1.Name = "memoEdit1";
-            this.memoEdit1.Size = new System.Drawing.Size(337, 142);
-            this.memoEdit1.TabIndex = 6;
-            // 
             // frmHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.memoEdit1);
-            this.Controls.Add(this.simpleButton1);
             this.Controls.Add(this.panelControl2);
-            this.Controls.Add(this.labelControl1);
             this.Name = "frmHome";
             this.Size = new System.Drawing.Size(670, 456);
             ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).EndInit();
             this.panelControl2.ResumeLayout(false);
             this.panelControl2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraEditors.PanelControl panelControl2;
         private DevExpress.XtraEditors.LabelControl labelControl2;
-        private DevExpress.XtraEditors.SimpleButton simpleButton1;
-        private DevExpress.XtraEditors.MemoEdit memoEdit1;
     }
 }
--- Teste/Teste/frmHome.cs
+++ Teste/Teste/frmHome.cs
@@ -16,7 +16,6 @@ namespace Teste
     public partial class frmHome : DevExpress.XtraEditors.XtraUserControl
     {
         private static frmHome _instance;
-
         public static frmHome Instance
         {
             get
@@ -31,25 +30,6 @@ namespace Teste
         public frmHome()
         {
             InitializeComponent();
-        }
-
-        private void GetSerialPort()
-        {
-            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'"))
-            {
-                var portnames = SerialPort.GetPortNames();
-                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
-
-                var portList = portnames.Select(n => n + " - " + ports.FirstOrDefault(s => s.Contains(n))).ToList();
-
-                foreach (string s in portList)
-                    memoEdit1.Text += s + "\r\n";
-            }
-        }
-
-        private void simpleButton1_Click(object sender, EventArgs e)
-        {
-            GetSerialPort();
         }
     }
 }
